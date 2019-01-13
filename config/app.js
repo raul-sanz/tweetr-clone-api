f9289f721e6832a27526c9dee8aa1cf13d129e10
raul sanz <estraulsanz@gmail.com>
2019-01-12 18:06:52 -0600

    success

--- config/app.js
+++ config/app.js
@@ -27,7 +27,8 @@ module.exports = {
   | to encrypted cookies, sessions and other sensitive data.
   |
   */
-  appKey: Env.getOrFail('APP_KEY'),
+  appKey: kL39Trglhy9Pn3sYFBxDK430tD3cF0ee,
+  // Env.getOrFail('APP_KEY'),
 
   http: {
     /*
